4bd18ec63e890cafcea2f5c348b27bb9d30e616f
AdrianGongora <adrianf8000@hotmail.com>
2021-10-06 00:05:42 -0500

    typecasting exercise

---
+++ Unit1/typeCasting.js
@@ -0,0 +1,18 @@
+// Adrian Gongora
+// First Exercise - Typecasting testing.
+
+// Dummy variables
+var num = '24';
+var num2 = 321
+var num3 = 3.14
+var num4 = '23.54 23.23 22.98';
+var word = 'Adrian';
+var word2 = 'z'
+
+console.log(Number(word)); // turns variables into numbers // the NaN is for not a number
+console.log(Boolean(word2)); // turn a variable into a boolean value (true or false)
+console.log(String(num2)); // turns a variable into a string
+
+console.log(num2.toString()); // Turns numbers into strings
+console.log(parseInt(num4)); //  returns the first natural or integer number in a string
+console.log(parseFloat(num4)); // returns a the first real or float number in a string
