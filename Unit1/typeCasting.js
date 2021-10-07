33111311af282ff0ef28eab8a71c738b307b71ec
AdrianGongora <adrianf8000@hotmail.com>
2021-10-07 08:52:48 -0500

    update class 07/10/2021

--- Unit1/typeCasting.js
+++ Unit1/typeCasting.js
@@ -9,10 +9,39 @@ var num4 = '23.54 23.23 22.98';
 var word = 'Adrian';
 var word2 = 'z'
 
-console.log(Number(word)); // turns variables into numbers // the NaN is for not a number
-console.log(Boolean(word2)); // turn a variable into a boolean value (true or false)
-console.log(String(num2)); // turns a variable into a string
+//Typecasting
+
+console.log(typeof Number(word)); // turns variables into numbers // the NaN is for not a number
+console.log(typeof Boolean(word2)); // turn a variable into a boolean value (true or false)
+console.log(typeof String(num2)); // turns a variable into a string
 
 console.log(num2.toString()); // Turns numbers into strings
 console.log(parseInt(num4)); //  returns the first natural or integer number in a string
 console.log(parseFloat(num4)); // returns a the first real or float number in a string
+
+//Falsy Values
+console.log(Boolean(""));
+console.log(Boolean(0));
+console.log(Boolean(NaN));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean(false));
+
+//True Values
+console.log(Boolean("Adrian"));
+console.log(Boolean(5));
+console.log(Boolean({key:"5"}));
+console.log(Boolean( [] ));
+
+console.log(typeof null);
+
+console.log(Number("5x") );
+
+console.log(Number.parseInt(num2));
+console.log(Number.parseFloat(num3));
+
+var result = "hola" + "mundo";
+var resultNumbersStringAdd = 2 + "22";
+var resultNumbersStringReminder = 2 - "22";
+console.log(resultNumbersStringAdd);
+console.log(resultNumbersStringReminder);
