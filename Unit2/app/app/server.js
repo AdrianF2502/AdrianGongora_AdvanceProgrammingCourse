73f352f454c770654c8fbd854af16db03ceaf9e8
AdrianGongora <adrianf8000@hotmail.com>
2021-12-04 02:24:31 -0600

    The last commit

---
+++ Unit2/app/app/server.js
@@ -0,0 +1,13 @@
+//const chalk = require("chalk");
+const express = require("express");
+
+const PORT = 8888;
+const app = express();
+
+app.get("/", (request, response) =>{
+        response.json("Hello world");
+})
+
+app.listen( PORT, () =>{
+        console.log((`http://localhost:${PORT}/`));
+})
